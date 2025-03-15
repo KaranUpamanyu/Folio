ef6b3ab3c13975d9b5b5ed81e9a25361364efd6c
Karan Upamanyu <karan.v.upamanyu@gmail.com>
2025-03-15 13:50:47 +0530

    feat: added content to the lyearn and springboard pages

--- src/app/lyearn/page.tsx
+++ src/app/lyearn/page.tsx
@@ -2,7 +2,70 @@ import React from "react";
 import GraphQLOptimizationCard from "@/components/Cards/Projects/GraphQLOptimization";
 import ChatbotCard from "@/components/Cards/Projects/Chatbot";
 import PageHeader from "@/components/PageHeader";
-import { LyearnContent } from "./const";
+
+const LyearnContent = (
+  <>
+    <p>
+      <span className="text-neutral-700">
+        <a
+          className="styled-link text-neutral-700 cursor-alias"
+          href="https://www.lyearn.com/"
+          target="_blank"
+        >
+          Lyearn
+        </a>
+      </span>{" "}
+      is an{" "}
+      <span className="text-neutral-700">employee experience platform</span> and
+      a <span className="text-neutral-700">B2B SaaS</span> company.
+    </p>
+    <p>
+      Lyearn’s features are divided into{" "}
+      <span className="text-neutral-700">four key verticals</span>:
+    </p>
+    <ul>
+      <li>
+        <span className="text-neutral-700">Align</span> – Set up meetings,
+        goals, and OKRs.
+      </li>
+      <li>
+        <span className="text-neutral-700">Learn</span> – Create and manage
+        courses, articles, quizzes, assignments, and sessions .
+      </li>
+      <li>
+        <span className="text-neutral-700">Perform</span> – Handle performance
+        reviews (like Lattice), ticket management (like Jira/Linear), and
+        habit/activity tracking .
+      </li>
+      <li>
+        <span className="text-neutral-700">Engage</span> – Facilitate surveys,
+        communities (like Slack), 1:1 meetings, praise walls, and feedback
+        requests/offers .
+      </li>
+    </ul>
+    <br />
+    <p>
+      Recently, Lyearn has been expanding into a{" "}
+      <span className="text-neutral-700">self-serve B2C platform</span> and
+      integrating{" "}
+      <span className="text-neutral-700">AI-powered knowledge management</span>
+      —both of which I’ve worked on extensively.
+    </p>
+    <br />
+    <p>
+      Lyearn has an amazing frontend codebase built and maintained at very high
+      standards, and a strong culture of staying{" "}
+      <span className="text-neutral-700">technically ahead</span> of the
+      competition.
+    </p>
+    <br />
+    <p>
+      I joined Lyearn in <span className="text-neutral-700">August 2022</span>{" "}
+      and I currently{" "}
+      <span className="text-neutral-700">lead the frontend team</span>.
+    </p>
+  </>
+);
 
 function LyearnPage() {
   return (
--- src/app/springboard/page.tsx
+++ src/app/springboard/page.tsx
@@ -1,7 +1,83 @@
 import React from "react";
+import PerformanceOptimizationCard from "@/components/Cards/Projects/PerformanceOptimization";
+import PageHeader from "@/components/PageHeader";
+
+const SpringboardContent = (
+  <>
+    <p>
+      <span className="text-neutral-700">
+        <a
+          className="styled-link text-neutral-700 cursor-alias"
+          href="https://www.springboard.com/"
+          target="_blank"
+        >
+          Springboard
+        </a>{" "}
+        is an online learning platform
+      </span>{" "}
+      that helps people—often from non-technical backgrounds—transition into
+      higher-paying tech jobs with a job guarantee. Their courses are long-term,
+      typically lasting{" "}
+      <span className="text-neutral-700">between six months to a year</span>,
+      and cover fields like{" "}
+      <span className="text-neutral-700">
+        data analytics, data science, machine learning, cybersecurity, software
+        development, and UI/UX design
+      </span>
+      .
+    </p>
+    <br />
+    <p>
+      During my final year of college, after{" "}
+      <span className="text-neutral-700">
+        securing strong job offers through campus placements
+      </span>
+      , I decided to help others do the same. I conducted{" "}
+      <a
+        className="styled-link text-neutral-700 cursor-alias"
+        href="https://karanupamanyu.github.io/placementcourse/"
+        target="_blank"
+      >
+        placement training programs
+      </a>{" "}
+      for <span className="text-neutral-700">over 50 students</span> across
+      Bengaluru.
+    </p>
+    <br />
+    <p>
+      Later, I reconnected with a senior from college—someone who had taught me
+      how to host websites when I was building a news/blogging site during my
+      first year of college. He had recently joined Springboard and spoke highly
+      of the company, which convinced me to apply. They were impressed with my
+      experience helping students get technical jobs, and to me,{" "}
+      <span className="text-neutral-700">
+        Springboard felt like what I had hoped to build with my own placement
+        courses, but on a much bigger scale
+      </span>
+      . After graduating, I joined the company.
+    </p>
+    <br />
+    <p>
+      I worked at Springboard for{" "}
+      <span className="text-neutral-700">two years</span>, from
+      <span className="text-neutral-700"> August 2022 to July 2024</span>.
+    </p>
+  </>
+);
 
 function SpringboardPage() {
-  return <div>SpringboardPage</div>;
+  return (
+    <>
+      <PageHeader
+        title="Springboard"
+        type="Work"
+        subtitle={SpringboardContent}
+      />
+      <div className="grid grid-cols-1 sm:grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1">
+        <PerformanceOptimizationCard />
+      </div>
+    </>
+  );
 }
 
 export default SpringboardPage;
